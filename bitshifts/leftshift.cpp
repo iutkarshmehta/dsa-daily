635b39981c85e21ffcf97266223a629cb9ed0501
Utkarsh  Mehta <utkarshmehta4991@gmail.com>
2024-12-01 21:06:43 +0530

    starting dsa

---
+++ bitshifts/leftshift.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a = 10;
+    //left shift
+    //a = a << 1;
+    a = a << 2;//shifts twice
+    cout << a << endl;
+
+    int b = 2;
+    b = b >> 1;
+    cout << b << endl;
+}
---
+++ pre_post_increment/prepost.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    int a = 10;
+    cout<< ++a<< endl;
+    cout << a-- << endl;
+    cout<< a++ + 1 << endl;//ans 7
+    cout << a++ << " " << --a << endl;
+    int c = 5;
+    cout<< (++c) * (++c)<< endl;
+
+}
