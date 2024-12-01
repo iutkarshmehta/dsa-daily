46997e678ba38a42f3fa2b2e1d71ef621c4fa972
Utkarsh  Mehta <utkarshmehta4991@gmail.com>
2024-12-02 00:08:46 +0530

    function

---
+++ function/sum.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b){
+    return a+b;
+}
+
+int main(){
+
+    int a;
+    cin >> a;
+
+    int b;
+    cin >> b;
+
+    int sumValue = sum(a,b);
+    cout<< "The value of sum is: "<< sumValue << endl;
+    return 0;
+
+} 
