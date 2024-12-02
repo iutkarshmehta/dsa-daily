d6487bf22d461faf297c438b50eb16c7dca49729
Utkarsh  Mehta <utkarshmehta4991@gmail.com>
2024-12-02 16:35:19 +0530

    comments added

--- number-system/decimal-to-binary.cpp
+++ number-system/decimal-to-binary.cpp
@@ -1,18 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/// @brief converts decimal to binary using division method
+/// @param n number 
+/// @return binary number
 int decimalToBinaryM1(int n) {
     int i = 0;
     int binaryNumber = 0;
     while(n>0){
-        int bit=n%2;
+        int bit = n % 2;
         binaryNumber = bit * pow(10, i++) + binaryNumber;
-        n = n/2;
+        n = n / 2;
 
     }
     return binaryNumber;
 }
-
+/// @brief decimal to binary using bit operation
+/// @param n number 
+/// @return binary number
 int decimalToBinaryM2(int n) {
     int i = 0;
     int binaryNumber = 0;
@@ -25,9 +30,23 @@ int decimalToBinaryM2(int n) {
     return binaryNumber;
 } 
 
+/// @brief converts binary number to decimal
+/// @param n is the decimal number
+/// @return decimal number of binary
+int binaryToDecimal(int n){
+    int decimalNumber = 0;
+    int i = 0;
+    while(n > 0){
+        int bit = n % 10;
+        decimalNumber = bit * pow(2, i++) + decimalNumber;
+        n /= 10;
+    }
+    return decimalNumber;
+}
+
 int main(){
     int n;
     cin>>n;
-    int binaryNumber = decimalToBinaryM2(n);
-    cout<<"The binary number is: "<< binaryNumber << endl;
+    int decimalNumber = binaryToDecimal(n);
+    cout<<"The decimal number is: "<< decimalNumber << endl;
 }
