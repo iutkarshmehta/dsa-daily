8555a2cb09d2280824d6d9351c5498262ac72705
Utkarsh  Mehta <utkarshmehta4991@gmail.com>
2024-12-25 13:49:13 +0530

    bubble sort

---
+++ sorting/bubble_sort.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/// @brief select the max and move to the extreme right, complextiy o(n2)
+/// @param arr
+void bubbleSort(vector<int> &arr)
+{
+    // code here
+    for (int i = arr.size() - 1; i >= 0; i--)
+    {
+        for (int j = 0; j <= i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    bubbleSort(arr);
+    for (auto i : arr)
+    {
+        cout << i << " ";
+    }
+}
