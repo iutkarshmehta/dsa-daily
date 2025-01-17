bfb2832372f76971c733ee5ee7a109c2cd7d5539
Utkarsh  Mehta <utkarshmehta4991@gmail.com>
2025-01-18 03:22:33 +0530

    Accepted code updation

--- arrays/gfg/union-of-two-using-duplicates.cpp
+++ arrays/gfg/union-of-two-using-duplicates.cpp
@@ -9,54 +9,80 @@ class Solution {
   public:
     // a,b : the arrays
     // Function to return a list containing the union of the two arrays.
+    // vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // Your code here
+        // return vector with correct order of elements
+    //     int s1 = a.size();
+    //     int s2 = b.size();
+    //     vector<int> unionArr;
+    //     int i = 0;
+    //     int j = 0;
+        
+        
+    //     while(i<s1 && j < s2) {
+    //         if(a[i] < b[j]) {
+    //             if(unionArr.empty() ||
+    //             unionArr.back() != a[i]) {
+    //                 unionArr.push_back(a[i]);
+    //                 i++;
+    //             }
+    //         } else if(a[i] > b[j]){
+    //             if(unionArr.empty() ||
+    //             unionArr.back() != b[j]) {
+    //                 unionArr.push_back(b[j]);
+    //                 j++;
+    //             }
+    //         } else {  
+    //             if (unionArr.empty() || unionArr.back() != a[i]) {
+    //                 unionArr.push_back(a[i]);
+    //             }
+    //             i++;
+    //             j++;
+    //         }
+    //     }
+        
+    //     while(j < s2) {
+    //         if(unionArr.size() == 0 ||
+    //             unionArr.back() != b[j]) {
+    //                 unionArr.push_back(b[j]);
+    //                 j++;
+    //         } 
+    //     }
+        
+    //     while (i < s1) {
+    //         if(unionArr.size() == 0 ||
+    //             unionArr.back() != a[i]) {
+    //                 unionArr.push_back(a[i]);
+    //                 i++;
+    //             }
+    //     }
+    //     return unionArr;
+    // }
+
+///This is the accepted code of GFG by using SET Dta structure
     vector<int> findUnion(vector<int> &a, vector<int> &b) {
         // Your code here
         // return vector with correct order of elements
         int s1 = a.size();
         int s2 = b.size();
-        vector<int> unionArr;
+        set<int> unionArr;
+        vector<int> ans;
         int i = 0;
-        int j = 0;
         
         
-        while(i<s1 && j < s2) {
-            if(a[i] < b[j]) {
-                if(unionArr.empty() ||
-                unionArr.back() != a[i]) {
-                    unionArr.push_back(a[i]);
-                    i++;
-                }
-            } else if(a[i] > b[j]){
-                if(unionArr.empty() ||
-                unionArr.back() != b[j]) {
-                    unionArr.push_back(b[j]);
-                    j++;
-                }
-            } else {  
-                if (unionArr.empty() || unionArr.back() != a[i]) {
-                    unionArr.push_back(a[i]);
-                }
-                i++;
-                j++;
-            }
+        for(int i = 0;i<s1;i++) {
+            unionArr.insert(a[i]);
+        }
+         
+        for(int j =0; j < s2;j++) {
+            unionArr.insert(b[j]);
         }
         
-        while(j < s2) {
-            if(unionArr.size() == 0 ||
-                unionArr.back() != b[j]) {
-                    unionArr.push_back(b[j]);
-                    j++;
-            } 
+        for(auto it: unionArr) {
+            ans.push_back(it);
+            i++;
         }
-        
-        while (i < s1) {
-            if(unionArr.size() == 0 ||
-                unionArr.back() != a[i]) {
-                    unionArr.push_back(a[i]);
-                    i++;
-                }
-        }
-        return unionArr;
+        return ans;
     }
 };
 
