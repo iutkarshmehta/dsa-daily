03ac6ee27dd9f63889eeaed22e293d31b201d080
Utkarsh  Mehta <utkarshmehta4991@gmail.com>
2025-01-24 12:01:44 +0530

    0s 1s 2s sort

--- arrays/sort-0s-1s-2s.cpp
+++ arrays/sort-0s-1s-2s.cpp
@@ -1,10 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> sort0s1s2s(vector<int> arr) {
-    
-}
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.size() - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
 
-int main() {
-
-}
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
