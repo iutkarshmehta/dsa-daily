809218c0c46c0bfa18473404c648d246c942679d
utkarsh <utkarshmehta4991@gmail.com>
2025-06-18 01:27:26 +0530

    formatting

--- arrays-babbar/reverse-array.cpp
+++ arrays-babbar/reverse-array.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 void reverseArray(vector<int> arr)
 {
-    int left = 0;
-    int right = arr.size() - 1;
-    while (left <= right)
-    {
-        swap(arr[left], arr[right]);
-        left++;
-        right--;
-    }
+   int left = 0;
+   int right = arr.size() - 1;
+   while (left <= right)
+   {
+      swap(arr[left], arr[right]);
+      left++;
+      right--;
+   }
 
-    cout << "Array elements are: ";
-    for (int i = 0; i < arr.size(); i++)
-    {
-        cout << arr[i] << " ";
-    }
+   cout << "Array elements are: ";
+   for (int i = 0; i < arr.size(); i++)
+   {
+      cout << arr[i] << " ";
+   }
 }
 int main()
 {
-    vector<int> arr = {10, 20, 30, 40, 50, 60};
-    reverseArray(arr);
-    return 0;
+   vector<int> arr = {10, 20, 30, 40, 50, 60};
+   reverseArray(arr);
+   return 0;
 }
