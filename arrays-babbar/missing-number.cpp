62e2dc197d00c67b74996d98872ed3f7f3ca1620
utkarsh <utkarshmehta4991@gmail.com>
2025-06-25 23:54:47 +0530

    issue in logic

--- arrays-babbar/missing-number.cpp
+++ arrays-babbar/missing-number.cpp
@@ -1,28 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int missingNumber(vector<int>& nums) {
-        int i= 0;
-        int ans = 0;
-        while(i< nums.size()) {
-            int index = nums[i] -1;
-            if(nums[i] != nums[index]) {
-                swap(nums[i], nums[index]);
-            } else {
-                i++;
-            }
+/// @brief issue in logic
+/// @param nums 
+/// @return 
+int missingNumber(vector<int> &nums)
+{
+    int i = 0;
+    int ans = 0;
+    while (i < nums.size())
+    {
+        int index = nums[i];
+        if ( index < nums.size() && nums[i] != nums[index])
+        {
+            swap(nums[i], nums[index]);
         }
-        for(int i = 0;i< nums.size() ;++i) {
-            if(nums[i] != i+1) {
-                ans =  i+1;
-            }
+        else
+        {
+            i++;
         }
-        return ans;
     }
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        if (nums[i] != i + 1)
+        {
+            ans = i + 1;
+        }
+    }
+    return ans;
+}
 
-    int main() {
-    vector<int> arr{0,3,3,4,1};
+int main()
+{
+    vector<int> arr{0, 3, 3, 4, 1};
     int ans = missingNumber(arr);
-    cout<< "Missing number is: " <<ans;
+    cout << "Missing number is: " << ans;
     return 0;
 }
