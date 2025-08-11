c5f36cc10c66a9d4339ea6f4ad887108d8f77f15
utkarsh <utkarshmehta4991@gmail.com>
2025-08-11 23:53:52 +0530

    format

--- arrays-babbar/strings/subsequence-one-subsequence.cpp
+++ arrays-babbar/strings/subsequence-one-subsequence.cpp
@@ -1,32 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool printS(int ind, vector<int> &ds, int s, int sum, int arr[], int n) {
-    if(ind == n) {
-        if(s == sum) {
-            for(auto it: ds) cout<< it<< " ";
-            cout<<endl;
+bool printS(int ind, vector<int> &ds, int s, int sum, int arr[], int n)
+{
+    if (ind == n)
+    {
+        if (s == sum)
+        {
+            for (auto it : ds)
+                cout << it << " ";
+            cout << endl;
             return true;
         }
-        else return false;
+        else
+            return false;
     }
 
     ds.push_back(arr[ind]);
     s += arr[ind];
-    if(printS(ind+1,ds,s,sum,arr,n) == true) {return true;};
+    if (printS(ind + 1, ds, s, sum, arr, n) == true)
+    {
+        return true;
+    };
     ds.pop_back();
-    s-=arr[ind];
+    s -= arr[ind];
 
-    //exclude
-    if(printS(ind+1,ds,s,sum,arr,n) == true) {return true;};
+    // exclude
+    if (printS(ind + 1, ds, s, sum, arr, n) == true)
+    {
+        return true;
+    };
     return false;
 }
 
-int main() {
-    int arr[] = {1,2,1};
-    int n =3;
+int main()
+{
+    int arr[] = {1, 2, 1};
+    int n = 3;
     int sum = 2;
     vector<int> ds;
-    bool returnValue = printS(0,ds,0,sum,arr, n);
+    bool returnValue = printS(0, ds, 0, sum, arr, n);
     return 0;
 }
--- arrays-babbar/strings/subsequence-sumRE.cpp
+++ arrays-babbar/strings/subsequence-sumRE.cpp
@@ -1,31 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void printS(int ind, vector<int> &ds, int s, int sum, int arr[], int n) {
-    if(ind == n) {
-        if(s == sum) {
-            for(auto it: ds) cout<< it<< " ";
-            cout<<endl;
+void printS(int ind, vector<int> &ds, int s, int sum, int arr[], int n)
+{
+    if (ind == n)
+    {
+        if (s == sum)
+        {
+            for (auto it : ds)
+                cout << it << " ";
+            cout << endl;
         }
         return;
     }
 
     ds.push_back(arr[ind]);
     s += arr[ind];
-    printS(ind+1,ds,s,sum,arr,n);
+    printS(ind + 1, ds, s, sum, arr, n);
     ds.pop_back();
-    s-=arr[ind];
+    s -= arr[ind];
 
-    //exclude
-    printS(ind+1,ds,s,sum,arr,n);
-
+    // exclude
+    printS(ind + 1, ds, s, sum, arr, n);
 }
 
-int main() {
-    int arr[] = {1,2,1};
-    int n =3;
+int main()
+{
+    int arr[] = {1, 2, 1};
+    int n = 3;
     int sum = 2;
     vector<int> ds;
-    printS(0,ds,0,sum,arr, n);
+    printS(0, ds, 0, sum, arr, n);
     return 0;
 }
