a9fadf16a215b1c6041e2d8a72bcbfb8030b1ebc
utkarsh <utkarshmehta4991@gmail.com>
2025-08-17 16:47:44 +0530

    insertion- deletion complete

--- linkedlist/linkedlist-deletion.cpp
+++ linkedlist/linkedlist-deletion.cpp
@@ -1,94 +1,219 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct Node{
-    public: 
+struct Node
+{
+public:
     int data;
-    Node* next;
+    Node *next;
 
-    public:
-    Node(int data1, Node* next1) {
+public:
+    Node(int data1, Node *next1)
+    {
         data = data1;
         next = next1;
     }
 
-    public:
-    Node(int data1) {
+public:
+    Node(int data1)
+    {
         data = data1;
         next = nullptr;
     }
-
 };
 
-Node* converttoLL(vector<int>& arr) {
-    Node* head = new Node(arr[0]);
-    Node* mover = head;
+Node *converttoLL(vector<int> &arr)
+{
+    Node *head = new Node(arr[0]);
+    Node *mover = head;
 
-    for(int i = 1; i < arr.size(); i++) {
-        Node* temp = new Node(arr[i]);
-        mover -> next = temp;
+    for (int i = 1; i < arr.size(); i++)
+    {
+        Node *temp = new Node(arr[i]);
+        mover->next = temp;
         mover = temp;
     }
     return head;
 }
-Node* deleteKthNode(Node* head, int k) {
-    if(head == nullptr) return head;
-    if(k==1) {
-        Node* temp = head;
-        head = head -> next;
+Node *deleteKthNode(Node *head, int k)
+{
+    if (head == nullptr)
+        return head;
+    if (k == 1)
+    {
+        Node *temp = head;
+        head = head->next;
         delete temp;
         return head;
     }
 
     int cnt = 0;
-    Node* temp = head;
-    Node* prev = nullptr;
+    Node *temp = head;
+    Node *prev = nullptr;
 
-    while(temp!=nullptr) {
-        
-        if(cnt == k) {
-            prev -> next = prev ->next->next;
+    while (temp != nullptr)
+    {
+        cnt++;
+        if (cnt == k)
+        {
+            prev->next = prev->next->next;
             delete temp;
             break;
         }
-        cnt++;
         prev = temp;
-        temp = temp -> next;
+        temp = temp->next;
     }
 
     return head;
-
 }
-Node* deleteStartNode(Node* head) {
-    Node* temp = head;
-    head = head -> next;
+Node *deleteStartNode(Node *head)
+{
+    Node *temp = head;
+    head = head->next;
     delete temp;
     return head;
 }
 
-Node* deleteTailNode(Node* head) {
-    Node* temp = head;
-    while(temp->next->next != nullptr) {
-        temp = temp -> next;
+Node *deleteTailNode(Node *head)
+{
+    Node *temp = head;
+    while (temp->next->next != nullptr)
+    {
+        temp = temp->next;
     }
-    delete temp -> next;
-    temp -> next = nullptr;
+    delete temp->next;
+    temp->next = nullptr;
+    return head;
+}
+Node *deleteElement(Node *head, int ele)
+{
+    if (head == nullptr)
+        return head;
+
+    if (head->data == ele)
+    {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        return head;
+    }
+
+    Node *temp = head;
+    Node *prev = nullptr;
+
+    while (temp != nullptr)
+    {
+        if (temp->data == ele)
+        {
+            prev->next = prev->next->next;
+            delete temp;
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
     return head;
 }
 
-int main() {
-    vector<int> v{1,2,3,4,5,6};
-    Node* head = new Node(v[0]);
-    Node* pt = converttoLL(v);
+Node *insertAtHead(Node *head, int ele)
+{
+    if (head == nullptr)
+    {
+        return new Node(ele);
+    }
+
+    return new Node(ele, head);
+}
+
+Node *insertAtTail(Node *head, int ele)
+{
+    if (head == nullptr)
+    {
+        return new Node(ele);
+    }
+
+    Node *temp = head;
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+
+    Node *newNode = new Node(ele);
+    temp->next = newNode;
+    return head;
+}
+
+Node *insertAtKthPosition(Node *head, int k, int ele)
+{
+    if (head == nullptr)
+    {
+        if (k == 1)
+        {
+            return new Node(ele);
+        }
+        else
+        {
+            return head;
+        }
+    }
+
+    if (k == 1)
+    {
+        return new Node(ele, head);
+    }
+
+    Node *temp = head;
+    int cnt = 0;
+    while (temp != nullptr)
+    {
+        cnt++;
+        if (cnt == k - 1)
+        {
+            Node *x = new Node(ele, temp->next);
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+
+Node* insertBeforeValue(Node* head, int ele, int val) {
+    if(head == nullptr) {
+        return nullptr;
+    }
+
+    if(head->data == val) {
+        return new Node(ele,head);
+    }
+
+    Node* temp = head;
+    while(temp->next != nullptr) {
+        if(temp->next->data == val) {
+            Node* x = new Node(ele,temp->next);
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+
+int main()
+{
+    vector<int> v{1, 2, 3, 4, 5, 6};
+    Node *head = new Node(v[0]);
+    Node *pt = converttoLL(v);
 
     // while( pt != nullptr) {
     //     cout<<pt -> data <<" ";
     //     pt = pt -> next;
     // }
 
-    Node* pt1 = deleteKthNode(pt,2);
-    while(pt1 != nullptr) {
-        cout<<pt1 -> data<< " ";
-        pt1 = pt1 -> next;
-    } 
+    Node *pt1 = insertBeforeValue(pt, 3, 5);
+    while (pt1 != nullptr)
+    {
+        cout << pt1->data << " ";
+        pt1 = pt1->next;
+    }
     return 0;
 }
